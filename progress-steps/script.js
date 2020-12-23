3f37cb3d594fa5dd88a0c1946d5fa9f08a8689f3
mahmuttyildiz <mahmutt.yildiz@outlook.com>
2020-12-23 20:08:05 +0300

    progress steps css and functionality added

---
+++ progress-steps/script.js
@@ -0,0 +1,50 @@
+const progressLine = document.querySelector("#progressLine");
+const previousButton = document.querySelector("#previousButton");
+const nextButton = document.querySelector("#previousButton");
+const progressCircles = document.querySelectorAll(".progressCircle");
+
+let currentActive = 1;
+
+const updateFunction = () => {
+  progressCircles.forEach((item, index) => {
+    if (index < currentActive) {
+      item.classList.add("active");
+    } else {
+      item.classList.remove("active");
+    }
+  });
+
+  const actives = document.querySelectorAll(".active");
+
+  progressLine.style.width =
+    ((actives.length - 1) / (progressCircles.length - 1)) * 100 + "%";
+
+  if (currentActive === 1) {
+    previousButton.disabled = true;
+  } else if (currentActive === progressCircles.length) {
+    nextButton.disabled = true;
+  } else {
+    previousButton.disabled = false;
+    nextButton.disabled = false;
+  }
+};
+
+nextButton.addEventListener("click", () => {
+  currentActive++;
+
+  if (currentActive > progressCircles.length) {
+    currentActive = progressCircles.length;
+  }
+
+  updateFunction();
+});
+
+previousButton.addEventListener("click", () => {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  updateFunction();
+});
