4d416d7eaae2a88d8dba293a9157169ad6cb8462
mahmuttyildiz <mahmutt.yildiz@outlook.com>
2021-01-22 20:51:00 +0300

    add dad jokes css and js files

---
+++ dad-jokes/script.js
@@ -0,0 +1,20 @@
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+const generateJoke = async () => {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  const res = await fetch("https://icanhazdadjoke.com", config);
+
+  const data = await res.json();
+
+  jokeEl.innerHTML = data.joke;
+};
+
+jokeBtn.addEventListener("click", generateJoke);
+
+generateJoke();
