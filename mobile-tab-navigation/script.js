972cc8ac135c59d4618e928821641717ba318e79
mahmuttyildiz <mahmutt.yildiz@outlook.com>
2021-02-26 21:28:08 +0300

    add mobile tab navigation functions

---
+++ mobile-tab-navigation/script.js
@@ -0,0 +1,20 @@
+const contents = document.querySelectorAll(".content");
+const listItems = document.querySelectorAll("nav ul li");
+
+const hideAllContents = () => {
+  contents.forEach((content) => content.classList.remove("show"));
+};
+
+const hideAllItems = () => {
+  listItems.forEach((item) => item.classList.remove("active"));
+};
+
+listItems.forEach((item, idx) => {
+  item.addEventListener("click", () => {
+    hideAllContents();
+    hideAllItems();
+
+    item.classList.add("active");
+    contents[idx].classList.add("show");
+  });
+});
